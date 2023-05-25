b43890ed76e055d1ea3f038b8b037a63e55ddfd1
niko.brusselaers <niko.brusselaers@student.ehb.be>
2023-05-25 14:14:46 +0200

    chore: removed unused layouts and nav partials

--- medialab_loan_application/resources/views/partials/nav/adminNav.blade.php
+++
@@ -1,7 +0,0 @@
-@yield("navigation")
-<nav class="nav bg-dark">
-    <a href="#" class="nav-item nav-link m-3 ">Home</a>
-    <a href="{{route("admin.index")}}" class="nav-item nav-link m-3" >Admin</a>
-
-    <a href="#" class="nav-item nav-link m-3">Loan System</a>
-</nav>
--- medialab_loan_application/resources/views/partials/nav/lendingNav.blade.php
+++
@@ -1,8 +0,0 @@
-@yield("navigation")
-<nav class="nav bg-dark">
-    <a href="#" class="nav-item nav-link m-3 ">Home</a>
-    <a href="{{route("inventoryManagement.index")}}" class="nav-item nav-link m-3" >Inventory Management</a>
-    <a href="{{route("inventoryManagement.create")}}" class="nav-item nav-link m-3" >create item</a>
-
-    <a href="#" class="nav-item nav-link m-3">Loan System</a>
-</nav>

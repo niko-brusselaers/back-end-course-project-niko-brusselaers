0587f1c9847506f6b351cd1c3a37a0834cb72fb4
niko.brusselaers <niko.brusselaers@student.ehb.be>
2023-05-26 17:01:31 +0200

    style: updated styling of error message

--- medialab_loan_application/resources/views/partials/error.blade.php
+++ medialab_loan_application/resources/views/partials/error.blade.php
@@ -1,5 +1,5 @@
 @foreach($errors->all() as $error)
-    <div class="alert alert-danger m-t-5" role="alert">
+    <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
         - {{$error}}
     </div>
 @endforeach
